221d564c2d274399786b1546b194d9fb9a46712c
Erfan Reed <erfan.reed@gmail.com>
2026-09-12 08:19:50 -0600

    package update.

--- docs/package/in-meeting.rb
+++ docs/package/in-meeting.rb
@@ -1,6 +1,6 @@
 cask "in-meeting" do
-  version "1.0.1"
-  sha256 "fc9049d149d85d3a0ea780f9d147ff7c5fc98df06332f49c7044fcbbe598871d"
+  version "1.1.0"
+  sha256 "cc530a9d5494b925e003810b75d5b86c9dfb9baea7f043dafc00ff861f53f884"
 
   url "https://github.com/fellowgeek/in-meeting/releases/download/v#{version}/in-meeting.dmg"
   name "in-meeting"
@@ -12,6 +12,7 @@ cask "in-meeting" do
   app "In Meeting.app"
 
   zap trash: [
+    "~/.in-meeting",
     "~/Library/Application Support/in-meeting",
     "~/Library/Preferences/com.fellowgeek.In-Meeting.plist",
     "~/Library/Saved Application State/com.fellowgeek.In-Meeting.savedState",
